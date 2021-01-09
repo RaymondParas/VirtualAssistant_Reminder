ef25277281340c805e3220e19b245d2514b0a45f
Jeff <rayparas98@gmail.com>
2021-01-09 15:22:02 -0500

    Created class for virtual assistant

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ engine = sr.setup_engine()
 # Setup microphone and listen
 mic = sr.setup_microphone()
 
+# Create virtual assistant
+assistant = sr.create_virtual_assistant(listener, mic, engine)
+assistant.introduce()
+
 # Retrieve and execute command
 while 1:
-    action = sr.get_reminder_action(listener, mic)
-    reminders = sr.execute_action(listener, mic, engine, action)
+    action = sr.get_reminder_action(assistant)
+    reminders = sr.execute_action(assistant, action)
     print(reminders)
     engine.say(reminders)
     engine.runAndWait()
